4d17c14863f3c61306cf5139cb40ce58f781430f
Scott Tesler <scottiet123@gmail.com>
2018-08-25 23:58:23 +0000

    Create initial version of website

---
+++ pages/_document.js
@@ -0,0 +1,40 @@
+import Document, { Head, Main, NextScript } from "next/document";
+import React from "react";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta charset="utf-8" />
+          <meta
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            name="viewport"
+          />
+          <link
+            rel="shortcut icon"
+            type="image/x-icon"
+            href="/static/favicon.ico"
+          />
+          <link
+            crossorigin="anonymous"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+            integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+            rel="stylesheet"
+          />
+          <link
+            crossorigin="anonymous"
+            href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+            integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+            rel="stylesheet"
+          />
+          <title>Read To Me</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,136 @@
-export default () => <div>Welcome to next.js!</div>;
+const _get = require("lodash/get");
+import fetch from "isomorphic-unfetch";
+import querystring from "querystring";
+import Head from "next/head";
+import React from "react";
+const ReactMarkdown = require("react-markdown");
+import {
+  Alert,
+  Button,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label
+} from "reactstrap";
+
+const ERROR_MESSAGE_DEFAULT = "An error occurred.";
+
+class Index extends React.Component {
+  state = {
+    articleMarkdown: "",
+    articleUrl: "",
+    audioUrl: "",
+    errorMessage: "",
+    isLoading: false
+  };
+
+  handleArticleUrlChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+
+    let articleMarkdown = "";
+    let audioUrl = "";
+    let errorMessage = "";
+
+    this.setState(
+      { articleMarkdown, audioUrl, errorMessage, isLoading: true },
+      async () => {
+        if (this.state.articleUrl.trim().length === 0) {
+          this.setState({ isLoading: false });
+          return;
+        }
+
+        try {
+          const httpOptions = {
+            body: JSON.stringify({ url: this.state.articleUrl }),
+            headers: { "Content-Type": "application/json" },
+            method: "POST"
+          };
+
+          const articleJson = await (await fetch(
+            "https://bf254ucpeg.execute-api.us-east-1.amazonaws.com/dev/convert-article-to-markdown",
+            httpOptions
+          )).json();
+
+          const title = articleJson.title;
+          const markdown = articleJson.markdown;
+
+          if (markdown.trim().length) {
+            articleMarkdown = `# ${title}\n${markdown}`;
+          }
+
+          audioUrl = (await (await fetch(
+            "https://bf254ucpeg.execute-api.us-east-1.amazonaws.com/dev/convert-article-to-audio",
+            httpOptions
+          )).json()).url;
+        } catch (err) {
+          errorMessage = ERROR_MESSAGE_DEFAULT;
+        }
+
+        this.setState({
+          articleMarkdown,
+          audioUrl,
+          errorMessage,
+          isLoading: false
+        });
+      }
+    );
+  };
+
+  render() {
+    const errorComponent = this.state.errorMessage.length ? (
+      <Alert color="danger">{this.state.errorMessage}</Alert>
+    ) : null;
+
+    const audioComponent = this.state.audioUrl.length ? (
+      <div style={{ padding: "2rem 0", textAlign: "center" }}>
+        <audio autoPlay controls src={this.state.audioUrl}>
+          Your browser does not support the <code>audio</code> element.
+        </audio>
+      </div>
+    ) : null;
+
+    const articleComponent = this.state.articleMarkdown.length ? (
+      <ReactMarkdown source={this.state.articleMarkdown} />
+    ) : null;
+
+    const buttonText = this.state.isLoading ? (
+      <i className="fas fa-spinner fa-spin" />
+    ) : (
+      "Submit"
+    );
+
+    return (
+      <Container>
+        <div style={{ margin: "0 auto", paddingTop: "3%", width: "90%" }}>
+          {errorComponent}
+          <Form onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <Label for="articleUrl">
+                <strong>Enter an article URL:</strong>
+              </Label>
+              <Input
+                id="articleUrl"
+                name="articleUrl"
+                onChange={this.handleArticleUrlChange}
+                type="text"
+                value={this.state.articleUrl}
+              />
+            </FormGroup>
+            <FormGroup style={{ textAlign: "center" }}>
+              <Button type="submit">{buttonText}</Button>
+            </FormGroup>
+          </Form>
+          {audioComponent}
+          {articleComponent}
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default Index;

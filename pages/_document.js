59e346e1b1ece7c1184ebbdb8d6d3adda5d7fa48
scotttesler <scottiet123@gmail.com>
2018-08-29 15:10:06 -0400

    Fix React attribute names

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <meta charset="utf-8" />
+          <meta charSet="utf-8" />
           <meta
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
             name="viewport"
@@ -17,13 +17,13 @@ export default class MyDocument extends Document {
             href="/static/favicon.ico"
           />
           <link
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
             integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
             rel="stylesheet"
           />
           <link
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
             href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
             integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
             rel="stylesheet"

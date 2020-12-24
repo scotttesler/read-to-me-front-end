e6bec42a8b2e8041419af58db3a456aacf5897a3
scotttesler <scottiet123@gmail.com>
2020-12-24 00:11:58 -0500

    Remove favicon and update Bootstrap

--- pages/_document.js
+++ pages/_document.js
@@ -1,30 +1,14 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html>
         <Head>
-          <meta charSet="utf-8" />
-          <meta
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-            name="viewport"
-          />
-          <link
-            href="/favicon.ico"
-            key="favicon"
-            rel="shortcut icon"
-            type="image/x-icon"
-          />
-          <link
+          <link 
             crossOrigin="anonymous"
-            href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/journal/bootstrap.min.css"
-            integrity="sha384-0d2eTc91EqtDkt3Y50+J9rW3tCXJWw6/lDgf1QNHLlV0fadTyvcA120WPsSOlwga"
+            href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/journal/bootstrap.min.css"
+            integrity="sha384-QDSPDoVOoSWz2ypaRUidLmLYl4RyoBWI44iA5agn6jHegBxZkNqgm2eHb6yZ5bYs"
             rel="stylesheet"
           />
         </Head>
